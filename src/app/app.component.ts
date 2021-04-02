85dbd2d9fcca51c55041e4ba12c1cfe07f07df85
amitthakur2013 <amitthakur2013@gmail.com>
2021-04-02 21:04:37 +0530

    my first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog-angular';
+  name='Amit';
+  getName(name){
+  	alert(name);
+  };
+  obj={
+  name:"peter"
+  };
+  site=window.location.href;
+  myevent(evt) {
+  console.warn(evt);
+  };
+  disabledBox=true;
+  enableBox(){
+  	this.disabledBox=!this.disabledBox;
+  };
+  show="green";
+  color="blue";
+  data1=['anil','peter','bruce','sunny'];
+  data=[
+  {
+  	name:'anil',
+  	age:20
+  },
+  {
+  name:'bruce',
+  age:36
+  },
+  {
+  name:'amit',
+  age:23
+  }
+  ]
+
 }
