a78cd8dcacd07b942c4af38b14925651fb57aa9c
amitthakur2013 <amitthakur2013@gmail.com>
2021-04-03 19:06:26 +0530

    done 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [{
+    type: 'success',
+    message: 'This is an success alert',
+  }, {
+    type: 'info',
+    message: 'This is an info alert',
+  }, {
+    type: 'warning',
+    message: 'This is a warning alert',
+  }, {
+    type: 'danger',
+    message: 'This is a danger alert',
+  }, {
+    type: 'primary',
+    message: 'This is a primary alert',
+  }, {
+    type: 'secondary',
+    message: 'This is a secondary alert',
+  }, {
+    type: 'light',
+    message: 'This is a light alert',
+  }, {
+    type: 'dark',
+    message: 'This is a dark alert',
+  }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'blog-angular';
   name='Amit';
@@ -46,6 +79,27 @@ export class AppComponent {
   changeColor(value){
   this.col_val=value.col.toString();
   console.warn(value);
+  };
+
+
+  alerts: Alert[];
+
+  constructor() {
+    this.reset();
   }
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  reset() {
+    this.alerts = Array.from(ALERTS);
+  };
+
+  model = {
+    left: true,
+    middle: false,
+    right: false
+  };
 
 }
